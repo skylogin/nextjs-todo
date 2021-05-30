10e4153d3fd1b033754a10f88cd19242e2d5ab0f
skylogin <skylogin@gmail.com>
2021-05-30 23:21:07 +0900

    add todo color

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 
 import styled from "styled-components";
 import palette from "../styles/palette";
@@ -8,17 +8,63 @@ import { TodoType } from "../types/todo";
 
 const Container = styled.div`
   width: 100%;
+
+  .todo-num {
+    margin-left: 12px;
+  }
   
   .todo-list-header {
     padding: 12px;
+    position: relative;
     border-bottom: 1px solid ${palette.gray};
-  }
-  .todo-list-last-todo{
-    font-size: 14px;
-    span{
-      margin-left: 8px;
+
+    .todo-list-last-todo{
+      font-size: 14px;
+      margin: 0 0 8px;
+      span{
+        margin-left: 12px;
+      }
+    }
+
+    .todo-list-header-colors {
+      display: flex;
+      .todo-list-header-color-num{
+        display: flex;
+        margin-right: 8px;
+        p {
+          font-size: 14px;
+          line-height: 16px;
+          margin: 0;
+          margin-left: 6px;
+        }
+        .todo-list-header-round-color {
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+        }
+      }
     }
   }
+
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
+  }
+
 `;
 
 
@@ -27,15 +73,55 @@ interface IProps {
 }
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
+
+  
+  type ObjectIndexType = {
+    [key: string]: number | undefined;
+  };
+
+
+  const todoColorNums = useMemo(() => {
+    // 색깔 객체 구하기
+    const colors: ObjectIndexType = {};
+    todos.forEach((todo) => {
+      const value = colors[todo.color];
+      if(!value){
+        // 기존에 없던 key(색깔)
+        colors[`${todo.color}`] = 1;
+      } else{
+        // 기존에 존재하던 key(색깔)
+        colors[`${todo.color}`] = value + 1;
+      }
+    });
+
+
+    return colors;
+  }, [todos]);
+
+
   return (
     <Container>
       <div className="todo-list-header">
         <p className="todo-list-last-todo">
           남은 To-Do<span>{todos.length}개</span>
         </p>
+        <div className="todo-list-header-colors">
+          {Object.keys(todoColorNums).map((color, index) => (
+            <div className="todo-list-header-color-num" key={index}>
+              <div className={`todo-list-header-round-color bg-${color}`} />
+              <p>{todoColorNums[color]}개</p>
+            </div>  
+          ))}
+        </div>
       </div>
     </Container>
   )
 }
+
+
+
+
+
+
 
 export default TodoList;
