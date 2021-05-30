301f352fff4d4dcc02158df6b76eac66e9b8fd54
skylogin <skylogin@gmail.com>
2021-05-30 22:54:24 +0900

    add type and add mock data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
+import React from "react";
 import { NextPage } from "next";
-import styled from "styled-components";
 
-const Container = styled.div`
-  padding: 20px;
-`;
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
+
+
+const todos: TodoType[] = [
+  {id: 1, text: "마트 가서 장보기", color: "red", checked: false },
+  {id: 2, text: "수학 숙제하기", color: "orange", checked: false },
+  {id: 3, text: "코딩하기", color: "yellow", checked: true },
+  {id: 4, text: "넥스트 공부하기", color: "green", checked: true },
+  {id: 5, text: "요리 연습하기", color: "blue", checked: false },
+  {id: 6, text: "분리수거 하기", color: "navy", checked: false },
+]
 
 const index: NextPage = () => {
-  return (
-    <Container>
-      <h1>hello styled-components!</h1>
-      <h2>hello styled-components!</h2>
-      <p>hello hello</p>
-      <ul>
-        <li>hello hello hello</li>
-      </ul>
-      <a>hello hello hello hello</a>
-      <span>TTTTTTTTTTTT</span>
-    </Container>
-  );
+  return <TodoList todos={todos} />;
 };
 
 export default index;
